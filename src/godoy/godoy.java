6ae3d958d73382126866ef9b72f5a725fa329f39
Barth Zalewski <barth.zalewski@spectos.com>
2016-10-30 12:10:37 +0100

    Initial commit

---
+++ src/godoy/godoy.java
@@ -0,0 +1,12 @@
+package godoy;
+
+import java.io.IOException;
+
+public class godoy {
+	
+	public static void main(String[] arguments) throws IOException {
+		int b = System.in.read();
+		System.out.println("Hello, world");
+	}
+	
+}
